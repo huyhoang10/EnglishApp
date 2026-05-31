2a9ef94bb72a4b3dc8ce33cb399be22ee6e4d2ef
ltaamlee <letam41225@gmail.com>
2026-05-31 10:57:48 +0700

    add file logo

--- app/src/main/java/com/example/efishapp/MainActivity.kt
+++ app/src/main/java/com/example/efishapp/MainActivity.kt
@@ -17,37 +17,37 @@ import com.example.efishapp.ui.theme.EfishAppTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // 1. Khởi tạo tầng Data (Firebase SDK)
-        val firebaseAuth = FirebaseAuth.getInstance()
-        val authRepository = AuthRepositoryImpl(firebaseAuth)
-
-        // 2. Khởi tạo tầng Domain (Các UseCases)
-        val loginUseCase = LoginUseCase(authRepository)
-        val registerUseCase = RegisterUseCase(authRepository)
-        val forgotPasswordUseCase = ForgotPasswordUseCase(authRepository)
-        val loginWithGoogleUseCase = LoginWithGoogleUseCase(authRepository)
-
-        // 3. Khởi tạo tầng Presentation (ViewModel)
-        // (Lưu ý: Cách khởi tạo trực tiếp này dùng để chạy ngay, thực tế sau này bạn nên dùng DI như Hilt/Koin)
-        val authViewModel = AuthViewModel(
-            loginUseCase = loginUseCase,
-            registerUseCase = registerUseCase,
-            forgotPasswordUseCase = forgotPasswordUseCase,
-            loginWithGoogleUseCase = loginWithGoogleUseCase
-        )
+//        // 1. Khởi tạo tầng Data (Firebase SDK)
+//        val firebaseAuth = FirebaseAuth.getInstance()
+//        val authRepository = AuthRepositoryImpl(firebaseAuth)
+//
+//        // 2. Khởi tạo tầng Domain (Các UseCases)
+//        val loginUseCase = LoginUseCase(authRepository)
+//        val registerUseCase = RegisterUseCase(authRepository)
+//        val forgotPasswordUseCase = ForgotPasswordUseCase(authRepository)
+//        val loginWithGoogleUseCase = LoginWithGoogleUseCase(authRepository)
+//
+//        // 3. Khởi tạo tầng Presentation (ViewModel)
+//        // (Lưu ý: Cách khởi tạo trực tiếp này dùng để chạy ngay, thực tế sau này bạn nên dùng DI như Hilt/Koin)
+//        val authViewModel = AuthViewModel(
+//            loginUseCase = loginUseCase,
+//            registerUseCase = registerUseCase,
+//            forgotPasswordUseCase = forgotPasswordUseCase,
+//            loginWithGoogleUseCase = loginWithGoogleUseCase
+//        )
         enableEdgeToEdge()
         setContent {
             EfishAppTheme {
-//                var vocabularies by remember { mutableStateOf<List<Vocabulary>?>(null) }
-//
-//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-//                    // 2. Bây giờ bạn có thể truyền innerPadding và vocabularys vào đây mà không bị lỗi
-//                    FlashcardScreen(
-//                        innerPadding = innerPadding,
-//                        vocabularies = vocabularies
-//                    )
-//                }
-                AuthNavGraph(viewModel = authViewModel)
+////                var vocabularies by remember { mutableStateOf<List<Vocabulary>?>(null) }
+////
+////                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+////                    // 2. Bây giờ bạn có thể truyền innerPadding và vocabularys vào đây mà không bị lỗi
+////                    FlashcardScreen(
+////                        innerPadding = innerPadding,
+////                        vocabularies = vocabularies
+////                    )
+////                }
+//                AuthNavGraph(viewModel = authViewModel)
             }
         }
     }
